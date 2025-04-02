0eedb61062cf7be285ad27536a74b045f63c0ca3
Gulnara <gulnarkaz@gmail.com>
2025-04-02 17:58:24 +0500

    fix: Configure static file settings and collect static files

--- shanyraq1/settings.py
+++ shanyraq1/settings.py
@@ -1,9 +1,5 @@
-"""
-Django settings for shanyraq1 project.
-"""
-
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,7 +99,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
