b35efcac2c0dc3d1f200234b71fd943669acefd5
Gulnara <gulnarkaz@gmail.com>
2025-04-02 15:08:23 +0500

    favicon path added

--- shanyraq1/urls.py
+++ shanyraq1/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.views import serve
+from django.urls import re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('shanyraks/', include('core.urls')),
+    re_path(r'^favicon\.ico$', serve, {'path': 'favicon.ico'}),
+
 ]
